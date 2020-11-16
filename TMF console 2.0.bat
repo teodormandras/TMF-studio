c21131b504fde6b82aa29db271a894c6c96d5450
teodormandras <70598976+teodormandras@users.noreply.github.com>
2020-11-16 17:38:42 +0200

    Create TMF console 2.0.bat

---
+++ TMF console 2.0.bat
@@ -0,0 +1,277 @@
+@ echo off
+title TMF console
+color 0
+
+:home
+echo TMFconsole//
+set /p imput=
+if %imput%==mkanim goto mkanim
+if %imput%==mkandroidapp goto mkandroidapp
+if %imput%==mkandroidapp:expert goto mkandroidapp:expert
+if %imput%==mkexewindows goto mkexewindows
+if %imput%==mkwebapp:Basic goto mkwebapp
+if %imput%==Console:CMD goto Console:CMD
+if %imput%==Project:WebApp goto Project:WebApp
+if %imput%==Project:VS goto Project:VS
+if %imput%==Basic:mkanim goto Basic:mkanim
+if %imput%==Speedtest goto Speedtest
+if %imput%==Project:Java goto Project:Java
+if %imput%==Project:JavaScript goto Project:JavaScript
+if %imput%==Apps:CDGS goto Apps:CDGS
+if %imput%==Project:Graphic goto Project:Graphic
+if %imput%==Apps:Chrome goto Apps:Chrome
+if %imput%==Games:Roblox goto Games:Roblox
+if %imput%==Games:King goto Games:King
+if %imput%==Games:CN goto Games:CN
+if %imput%==Apps:Whatsapp goto Apps:Whatsapp
+if %imput%==Apps:WinPCSIGN goto Apps:WinPCSIGN
+if %imput%==Services:CopyCenterTMF goto Services:CopyCenterTMF
+
+:mkanim
+echo starting maker....
+start https://www.animaker.com/
+goto home 
+cls
+
+:Basic:mkanim
+echo starting maker....
+start https://www.powtoon.com
+goto home 
+cls
+
+:mkandroidapp
+echo starting maker....
+start https://mobincube.com
+goto home 
+cls
+
+:mkandroidapp:expert
+echo starting maker....
+start https://ibuildapp.com
+goto home 
+cls
+
+:mkexewindows
+start iexpress.exe
+echo starting...
+goto home 
+cls
+
+:mkwebapp:Basic
+start https://sway.office.com
+echo starting...
+goto home 
+cls
+
+:Console:CMD
+start cmd.exe
+echo starting...
+goto home 
+cls
+
+:Project:WebApp
+start notepad.exe
+echo starting...
+goto home 
+cls
+
+
+:Project:VS
+start https://visualstudio.microsoft.com
+echo starting downloader VS...
+goto home 
+cls
+
+
+
+:Basic:mkanim
+echo starting maker....
+start https://www.powtoon.com
+goto home 
+cls
+
+:mkandroidapp
+echo starting maker....
+start https://mobincube.com
+goto home 
+cls
+
+:mkandroidapp:expert
+echo starting maker....
+start https://ibuildapp.com
+goto home 
+cls
+
+:mkexewindows
+start iexpress.exe
+echo starting...
+goto home 
+cls
+
+:mkwebapp:Basic
+start https://sway.office.com
+echo starting...
+goto home 
+cls
+
+:Console:CMD
+start cmd.exe
+echo starting...
+goto home 
+cls
+
+:Project:WebApp
+start notepad.exe
+echo starting...
+goto home 
+cls
+
+
+:Project:VS
+start https://visualstudio.microsoft.com
+echo starting downloader VS...
+goto home 
+cls
+
+
+
+
+:Project:Java
+start https://java.com
+echo starting downloader JAVA...
+goto home 
+cls
+
+:Project:JavaScript
+start https://javascript.com
+echo starting downloader JS...
+goto home 
+cls
+
+:Project:Graphic
+start https://coreldraw.com
+echo starting downloader...
+goto home 
+cls
+
+
+:Apps:Chrome
+start https://chrome.google.com
+echo starting downloader...
+goto home 
+cls
+
+
+:Apps:Firefox
+start https://firefox.com
+echo starting downloader...
+goto home 
+cls
+
+:Games:Roblox
+start https://roblox.com
+echo starting downloader...
+goto home 
+cls
+
+:Games:King
+start https://king.com
+echo starting downloader...
+goto home 
+cls
+
+:Games:CN
+start https://cngames.eu
+echo starting downloader...
+goto home 
+cls
+
+:Apps:Whatsapp
+start https://whatsapp.com
+echo starting...
+goto home 
+cls
+
+
+:Apps:WinPCSIGN
+start https://winpcsign.ca
+echo starting...
+goto home 
+cls
+
+:Services:CopyCenterTMF
+start https://copycentertmf.business.site
+echo starting...
+goto home 
+cls  
+
+:Speedtest
+echo Starting speedtet...
+start https://speedtest.net
+goto home 
+cls
+
+:Project:Java
+start https://java.com
+echo starting downloader JAVA...
+goto home 
+cls
+
+:Project:JavaScript
+start https://javascript.com
+echo starting downloader JS...
+goto home 
+cls
+
+:Project:Graphic
+start https://coreldraw.com
+echo starting downloader...
+goto home 
+cls
+
+
+:Apps:CDGS
+start https://corel.com
+echo starting downloader...
+goto home 
+cls
+
+:Games:Roblox
+start https://roblox.com
+echo starting downloader...
+goto home 
+cls
+
+:Games:King
+start https://king.com
+echo starting downloader...
+goto home 
+cls
+
+:Games:CN
+start https://cngames.eu
+echo starting downloader...
+goto home 
+cls
+
+:Apps:Whatsapp
+start https://whatsapp.com
+echo starting...
+goto home 
+cls
+
+
+
+
+:Services:CopyCenterTMF
+start https://copycentertmf.business.site
+echo starting...
+goto home 
+cls
+
+
+:Apps:WinPCSIGN
+start https://winpcsign.ca
+echo starting...
+goto home 
+cls
